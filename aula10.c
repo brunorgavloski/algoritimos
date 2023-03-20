25071c4440332fe3777161bb876ad992ac7e49b4
Bruno Gavloski <gavloski11@gmail.com>
2023-03-20 19:59:19 -0300

    aula11

---
+++ aula10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*	
+ *	Aula 10
+ *	lendo vários valores
+ *
+ */
+
+
+
+int main() {
+	int num1, num2, num3;
+
+	printf("Digite tres valores: ");
+	scanf("%d%d%d",&num1,&num2,&num3);
+
+
+	printf("Valores lidos: %d, %d, e %d:\n", num1, num2, num3);
+
+
+	return 0 ;
+
+}
