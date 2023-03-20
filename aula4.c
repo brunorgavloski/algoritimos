c06b384994ccf2e5d88901bc90d4a3cfcf6b0560
Bruno Gavloski <gavloski11@gmail.com>
2023-03-20 19:32:38 -0300

    aula4 - getchar

---
+++ aula4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+//lendo caracteres 
+//funcao getchar
+
+
+int main() {
+	
+	char letra;
+	
+	printf("digite uma letra: ");
+	letra = getchar();
+	
+	printf("Caracter lido: %c\n", letra);
+
+	return 0;
+
+}	
+		
+		
+
+
